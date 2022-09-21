4c8914ab5cd0068d186addbe8a437d5f962fc29b
Will Da Silva <will@willdasilva.xyz>
2022-09-21 13:51:07 -0400

    Setup pre-commit & address lint issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,5 +10,7 @@ sys.path.append(str(test_dir.parent))
 
 
 def pytest_sessionstart(session: Session) -> None:
-    if os.environ.get('CONTAINERIZED', 'False')[0].lower() != 't':
-        raise Exception('These tests should only be run within a container! Refer to README.md')
+    if os.environ.get("CONTAINERIZED", "False")[0].lower() != "t":
+        raise Exception(
+            "These tests should only be run within a container! Refer to README.md"
+        )
